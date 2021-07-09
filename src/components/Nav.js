5604c052474ba41238af45a5e811d34dee33f8c4
Euhid Aman <aman.derax20@gmail.com>
2021-07-09 13:26:24 +0530

    Added info abt style to remove underline from Link in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './Nav.css';
 import { Link } from "react-router-dom";
 
+// use this code snippet to remove underlines from Link tag -->
+//     style={ { textDecoration: 'none' } }
+
 const Nav = () => {
     return (
         <div >
             <ul className="nav">
-                <Link to="/">
+                <Link to="/" style={{ textDecoration: 'none' }}>
                     <li>Home</li>
                 </Link>
-                <Link to="/about">
+                <Link to="/about" style={{ textDecoration: 'none' }}>
                     <li>About</li>
                 </Link>
-                <Link to="/contact">
+                <Link to="/contact" style={{ textDecoration: 'none' }}>
                     <li>Contact</li>
                 </Link>
             </ul>
